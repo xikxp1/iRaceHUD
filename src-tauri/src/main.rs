cbae5189e82a9430c97bca75b595549bdd72aa4c
Ivan Kachalkin <xikxp1@gmail.com>
2024-07-02 23:43:19 +0300

    feat all: transparent overlay implementation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let window = app.get_window("main").expect("Failed to get main window");
+            let _ = window.set_ignore_cursor_events(true);
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
