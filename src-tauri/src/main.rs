e383833ddb0aa168231569b9eee13f294b9d6e73
Ivan Kachalkin <xikxp1@gmail.com>
2024-07-28 19:17:30 +0200

    feat backend: code cleanup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,13 +5,14 @@ use std::{sync::OnceLock, time::Duration};
 
 use anyhow::Result;
 use iracing_telem::{Client, DataUpdateResult};
-use log::{debug, info};
+use log::{debug, error, info};
 use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayMenu};
 use tauri_plugin_log::LogTarget;
 
 static WINDOW: OnceLock<tauri::Window> = OnceLock::new();
 
 struct TelemetryData {
+    active: bool,
     gear: String,
     speed: u32,
     rpm: u32,
@@ -66,15 +67,16 @@ fn main() {
 pub fn connect() -> Result<(), iracing_telem::Error> {
     let mut c = Client::new();
     loop {
-        debug!("start iRacing");
+        info!("Start iRacing");
         unsafe {
             match c.wait_for_session(Duration::new(600, 0)) {
                 None => {
-                    println!("remember to start iRacing!");
+                    info!("Remember to start iRacing!");
                     return Ok(());
                 }
                 Some(mut s) => {
                     let mut data = TelemetryData {
+                        active: false,
                         gear: String::from("N"),
                         speed: 0,
                         rpm: 0,
@@ -85,128 +87,121 @@ pub fn connect() -> Result<(), iracing_telem::Error> {
                         position: 1,
                         incidents: 0,
                     };
-                    let gear = s.find_var("Gear").unwrap();
-                    let speed = s.find_var("Speed").unwrap();
-                    let rpm = s.find_var("RPM").unwrap();
-                    let lap = s.find_var("Lap").unwrap();
-                    let lap_time = s.find_var("SessionTime").unwrap();
-                    let brake = s.find_var("Brake").unwrap();
-                    let throttle = s.find_var("Throttle").unwrap();
-                    let position = s.find_var("PlayerCarClassPosition").unwrap();
-                    let incidents = s.find_var("PlayerCarMyIncidentCount").unwrap();
+                    let gear = s.find_var("Gear").expect("Gear variable not found");
+                    let speed = s.find_var("Speed").expect("Speed variable not found");
+                    let rpm = s.find_var("RPM").expect("RPM variable not found");
+                    let lap = s.find_var("Lap").expect("Lap variable not found");
+                    let lap_time = s
+                        .find_var("SessionTime")
+                        .expect("SessionTime variable not found");
+                    let brake = s.find_var("Brake").expect("Brake variable not found");
+                    let throttle = s.find_var("Throttle").expect("Throttle variable not found");
+                    let position = s
+                        .find_var("PlayerCarClassPosition")
+                        .expect("PlayerCarClassPosition variable not found");
+                    let incidents = s
+                        .find_var("PlayerCarMyIncidentCount")
+                        .expect("PlayerCarMyIncidentCount variable not found");
                     loop {
                         match s.wait_for_data(Duration::from_millis(20)) {
                             DataUpdateResult::Updated => {
-                                if WINDOW.get().is_none() {
-                                    debug!("no main window");
+                                let window_opt = WINDOW.get();
+
+                                if window_opt.is_none() {
+                                    error!("Window is none");
                                     break;
                                 }
 
+                                let window = window_opt.expect("Window is none");
+
                                 // gear
                                 let raw_gear_value: i32 = s.value(&gear)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let gear_value = match raw_gear_value {
-                                        -1 => String::from("R"),
-                                        0 => String::from("N"),
-                                        _ => raw_gear_value.to_string(),
-                                    };
-                                    if gear_value != data.gear {
-                                        let _ = window.emit("gear", gear_value.clone());
-                                        data.gear = gear_value;
-                                    }
+                                let gear_value = match raw_gear_value {
+                                    -1 => String::from("R"),
+                                    0 => String::from("N"),
+                                    _ => raw_gear_value.to_string(),
+                                };
+                                if gear_value != data.gear {
+                                    let _ = window.emit("gear", gear_value.clone());
+                                    data.gear = gear_value;
                                 }
 
                                 // speed
                                 let raw_speed_value: f32 = s.value(&speed)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let speed_value = (raw_speed_value * 3.6).round() as u32;
-                                    if speed_value != data.speed {
-                                        let _ = window.emit("speed", speed_value);
-                                        data.speed = speed_value;
-                                    }
+                                let speed_value = (raw_speed_value * 3.6).round() as u32;
+                                if speed_value != data.speed {
+                                    let _ = window.emit("speed", speed_value);
+                                    data.speed = speed_value;
                                 }
 
                                 // rpm
                                 let raw_rpm_value: f32 = s.value(&rpm)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let rpm_value = raw_rpm_value.round() as u32;
-                                    if rpm_value != data.rpm {
-                                        let _ = window.emit("rpm", rpm_value);
-                                        data.rpm = rpm_value;
-                                    }
+                                let rpm_value = raw_rpm_value.round() as u32;
+                                if rpm_value != data.rpm {
+                                    let _ = window.emit("rpm", rpm_value);
+                                    data.rpm = rpm_value;
                                 }
 
                                 // lap
                                 let raw_lap_value: i32 = s.value(&lap)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let lap_value = raw_lap_value as u32;
-                                    if lap_value != data.lap {
-                                        let _ = window.emit("lap", lap_value);
-                                        data.lap = lap_value;
-                                    }
+                                let lap_value = raw_lap_value as u32;
+                                if lap_value != data.lap {
+                                    let _ = window.emit("lap", lap_value);
+                                    data.lap = lap_value;
                                 }
 
                                 // lap_time
                                 let raw_lap_time_value: f64 = s.value(&lap_time)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let lap_time_value =
-                                        Duration::from_secs_f64(raw_lap_time_value);
-                                    let _ = window.emit(
-                                        "lap_time",
-                                        format!(
-                                            "{}:{:02}:{:03}",
-                                            lap_time_value.as_secs() / 60,
-                                            lap_time_value.as_secs() % 60,
-                                            lap_time_value.subsec_millis()
-                                        ),
-                                    );
-                                    data.lap_time = lap_time_value;
-                                }
+                                let lap_time_value = Duration::from_secs_f64(raw_lap_time_value);
+                                let _ = window.emit(
+                                    "lap_time",
+                                    format!(
+                                        "{}:{:02}:{:03}",
+                                        lap_time_value.as_secs() / 60,
+                                        lap_time_value.as_secs() % 60,
+                                        lap_time_value.subsec_millis()
+                                    ),
+                                );
+                                data.lap_time = lap_time_value;
 
                                 // brake
                                 let raw_brake_value: f32 = s.value(&brake)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let brake_value = (raw_brake_value * 100.0).round() as u32;
-                                    let _ = window.emit("brake", brake_value);
-                                    data.brake = brake_value;
-                                }
+                                let brake_value = (raw_brake_value * 100.0).round() as u32;
+                                let _ = window.emit("brake", brake_value);
+                                data.brake = brake_value;
 
                                 // throttle
                                 let raw_throttle_value: f32 = s.value(&throttle)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let throttle_value =
-                                        (raw_throttle_value * 100.0).round() as u32;
-                                    let _ = window.emit("throttle", throttle_value);
-                                    data.throttle = throttle_value;
-                                }
+                                let throttle_value = (raw_throttle_value * 100.0).round() as u32;
+                                let _ = window.emit("throttle", throttle_value);
+                                data.throttle = throttle_value;
 
                                 // position
                                 let raw_position_value: i32 = s.value(&position)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let position_value = raw_position_value as u32;
-                                    if position_value != data.position {
-                                        let _ = window.emit("position", position_value);
-                                        data.position = position_value;
-                                    }
+                                let position_value = raw_position_value as u32;
+                                if position_value != data.position {
+                                    let _ = window.emit("position", position_value);
+                                    data.position = position_value;
                                 }
 
                                 // incidents
                                 let raw_incidents_value: i32 = s.value(&incidents)?;
-                                if let Some(window) = WINDOW.get() {
-                                    let incidents_value = raw_incidents_value as u32;
-                                    if incidents_value != data.incidents {
-                                        let _ = window.emit("incident_count", incidents_value);
-                                        data.incidents = incidents_value;
-                                    }
+                                let incidents_value = raw_incidents_value as u32;
+                                if incidents_value != data.incidents {
+                                    let _ = window.emit("incident_count", incidents_value);
+                                    data.incidents = incidents_value;
                                 }
                             }
                             DataUpdateResult::NoUpdate => {
-                                debug!("no update")
+                                debug!("No update")
                             }
                             DataUpdateResult::FailedToCopyRow => {
-                                debug!("too slow")
+                                debug!("Too slow")
                             }
-                            DataUpdateResult::SessionExpired => break,
+                            DataUpdateResult::SessionExpired => {
+                                info!("Session expired");
+                                break;
+                            }
                         }
                     }
                 }
